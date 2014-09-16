1626959314f30b0c9a50d98b95e127d5b9e9f5b3
gumingcn <gumingcn@gmail.com>
2014-09-16 17:41:16 +0800

    add new repo

--- src/main/java/org/jinn/typhoon/utils/TestRedis.java
+++ src/main/java/org/jinn/typhoon/utils/TestRedis.java
@@ -1,6 +1,6 @@
 package org.jinn.typhoon.utils;
 
-import org.jinn.redis.Redis4JHAServer;
+//import org.jinn.redis.Redis4JHAServer;
 import redis.clients.jedis.JedisPoolConfig;
 
 import java.util.ArrayList;
@@ -11,16 +11,16 @@ import java.util.List;
  */
 public class TestRedis {
     public static void main(String[] args) {
-        List servers = new ArrayList();
-        servers.add("127.0.0.1:6379:0");
-        servers.add("127.0.0.1:6379:0");
-        Redis4JHAServer server = new Redis4JHAServer();
-        server.setServers(servers);
-        server.setDoubleWrite(false);
-        server.setPoolConfig(new JedisPoolConfig());
-        System.out.println(server.set("258.cntrm", "10"));
-        System.out.println(server.get("258.cntrm"));
-        System.out.println(server.incr("258.cntrm"));
+//        List servers = new ArrayList();
+//        servers.add("127.0.0.1:6379:0");
+//        servers.add("127.0.0.1:6379:0");
+//        Redis4JHAServer server = new Redis4JHAServer();
+//        server.setServers(servers);
+//        server.setDoubleWrite(false);
+//        server.setPoolConfig(new JedisPoolConfig());
+//        System.out.println(server.set("258.cntrm", "10"));
+//        System.out.println(server.get("258.cntrm"));
+//        System.out.println(server.incr("258.cntrm"));
 //	        System.out.println(server.get("258.cntrm"));
 //     		Integer a=Integer.valueOf(1);
 //			System.out.println("Test:"+(a==b));
